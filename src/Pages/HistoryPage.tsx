0371e150aefaef841cd374dd31603cea261c7158
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-08-03 20:04:50 -0300

    reset history

--- src/Pages/HistoryPage.tsx
+++ src/Pages/HistoryPage.tsx
@@ -1,4 +1,4 @@
-import {CardMetion} from "../Components/CardMetion.tsx";
+// import {CardMetion} from "../Components/CardMetion.tsx";
 import {NavBar} from "../Components/NavBar.tsx";
 
 export const HistoryPage = () => {
@@ -7,21 +7,21 @@ export const HistoryPage = () => {
             <NavBar/>
 
             <div className="mt-24 flex flex-col w-full gap-2 justify-center items-center lg:flex-row">
-                <CardMetion
-                    name="Raissa"
-                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"
-                    grade={940}
-                />
-                <CardMetion
-                    name="Raissa"
-                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"
-                    grade={940}
-                />
-                <CardMetion
-                    name="Raissa"
-                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"
-                    grade={940}
-                />
+                {/*<CardMetion*/}
+                {/*    name="Raissa"*/}
+                {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"*/}
+                {/*    grade={940}*/}
+                {/*/>*/}
+                {/*<CardMetion*/}
+                {/*    name="Raissa"*/}
+                {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"*/}
+                {/*    grade={940}*/}
+                {/*/>*/}
+                {/*<CardMetion*/}
+                {/*    name="Raissa"*/}
+                {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam fuga molestias quae quisquam sapiente totam vel! Aliquam aspernatur delectus earum ipsam iste minus neque ratione repellendus veniam vero! Exercitationem, fugit?"*/}
+                {/*    grade={940}*/}
+                {/*/>*/}
             </div>
         </>
     )
